ba2ef9886645eb2edcaed8c139bf0765a45e5476
Siamion <sem_en264@mail.ru>
2018-03-06 13:12:12 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,181 +5,57 @@
 
 using namespace std;
 
-struct Element{
-    int value;
-    struct Element *next;
-};
+	class Queue{
+	public:
+	    int sizeQ;
+	    int topOfQueue;
+	    int *data;
+	};
 
-struct List{
-    int initializedL;
-    Element *head;
-};
+	void init(Queue& q, int size){
+        q.data = new int[size];
+        q.sizeQ = size;
+        q.topOfQueue = 0;
+	}
 
-void init(List& l){
-    l.initializedL = 1;
-    l.head = NULL;
-}
-
-void insertHead(List& l, int x){
-    if(l.initializedL == 1){
-        Element *newEl = new Element;
-        newEl->value = x;
-        newEl->next = l.head;
-        l.head = newEl;
-        newEl = NULL;
-    }
-}
-
-bool deleteHead(List& l, int &oldHead){
-    if(l.head!=NULL && l.initializedL == 1){
-        oldHead = l.head->value;
-        Element *oldHeadPtr = l.head;
-        l.head = l.head->next;
-        delete oldHeadPtr;
-        oldHeadPtr = NULL;
+	bool enqueue(Queue& q, int value){
+	    if(q.topOfQueue==q.sizeQ)
+            return false;
+        q.data[q.topOfQueue++] = value;
         return true;
-    }
-    return false;
-}
+	}
 
-void insertTail(List& l, int x){
-    if(l.initializedL == 1){
-        Element *newEl = new Element;
-        newEl->next = NULL;
-        newEl->value = x;
-        Element *elementInspected = l.head;
-        if(l.head!=NULL){
-            while(elementInspected->next!=NULL){
-                elementInspected = elementInspected->next;
-            }
-            elementInspected->next = newEl;
-        }else{
-            l.head = newEl;
+	bool dequeue(Queue& q, int &value){
+	    if(q.topOfQueue==0)
+            return false;
+        value = q.data[0];
+        for(int i = 0; i<q.topOfQueue-1; i++){
+            q.data[i] = q.data[i+1];
         }
-        newEl = NULL;
-    }
-}
-
-bool deleteTail(List& l, int &oldTail){
-    if(l.initializedL != 1)
-        return false;
-    if(l.head == NULL && l.initializedL == 1)
-        return false;
-    if(l.head->next == NULL && l.initializedL == 1){
-        oldTail = l.head->value;
-        delete l.head;
-        l.head = NULL;
+        q.topOfQueue--;
         return true;
-    }
-    Element *beforeInspectedElem = l.head;
-    Element *elementInspected = l.head->next;
-    while(elementInspected->next!=NULL){
-        beforeInspectedElem = elementInspected;
-        elementInspected = elementInspected->next;
-    }
-    oldTail = elementInspected->value;
-    delete elementInspected;
-    elementInspected = NULL;
-    beforeInspectedElem->next = NULL;
-    beforeInspectedElem = NULL;
-    return true;
-}
+	}
 
-int findPosOfValue(List& l, int value){
-    if(l.head!=NULL && l.initializedL == 1){
-        Element *elementInspected = l.head;
-        int i = 0;
-        while(elementInspected->next!=NULL){
-            if(elementInspected->value == value){
-                return i;
-            }
-            elementInspected = elementInspected->next;
-            i++;
-        }
-        if(elementInspected->value == value){
-            return i;
-        }
-    }
-	return -1;
-}
-
-bool deleteValue(List& l, int value){
-    if(l.head!=NULL && l.initializedL == 1){
-        if(l.head->value == value){
-            l.head = l.head->next;
+	bool isEmpty(Queue& q){
+	    if(q.topOfQueue==0)
             return true;
-        }
-        Element *elementInspected = l.head;
-        Element *beforeInspectedElem = NULL;
-        while(elementInspected->next!=NULL){
-            if(elementInspected->value == value){
-                beforeInspectedElem->next = elementInspected->next;
-                delete elementInspected;
-                return true;
-            }
-            beforeInspectedElem = elementInspected;
-            elementInspected = elementInspected->next;
-        }
-        if(elementInspected->value == value){
-            beforeInspectedElem->next = NULL;
-            delete elementInspected;
+        return false;
+	}
+
+	bool isFull(Queue& q){
+	    if(q.topOfQueue==q.sizeQ)
             return true;
-        }
-        beforeInspectedElem = elementInspected;
-        elementInspected = elementInspected->next;
-    }
-    return false;
-}
+        return false;
+	}
 
-bool atPosition(List& l, int pos, int &value){
-    if(l.head != NULL && l.initializedL == 1){
-        Element *elementInspected = l.head;
-        int i = 0;
-        while(elementInspected->next!=NULL){
-            if(i == pos){
-                value = elementInspected->value;
-                return true;
-            }
-            elementInspected = elementInspected->next;
-            i++;
-        }
-        if(i == pos){
-            value = elementInspected->value;
-            return true;
-        }
-    }
-	return false;
-}
+	void show(Queue& q){
+	    for(int i = 0; i<q.topOfQueue; i++){
+            cout << q.data[i] << ",";
+	    }
+	    cout << endl;
+	}
 
-void showListFromHead(List& l){
-    if(l.head!=NULL && l.initializedL == 1){
-        Element *elementInspected = l.head;
-        while(elementInspected->next!=NULL){
-            cout << elementInspected->value << ",";
-            elementInspected = elementInspected->next;
-        }
-        cout << elementInspected->value << "," << endl;
-    }else if(l.head == NULL){
-	    //If this does not want, try changing the value in  cout << "" << endl to " " or "\n" but i think it should work fine
-	     cout << "" << endl;
-    }
-}
 
-void clearList(List& l){
-    if(l.head!=NULL && l.initializedL == 1){
-        Element *elementInspected = l.head;
-        l.head = NULL;
-        Element *nextInspected = NULL;
-        while(elementInspected->next!=NULL){
-            nextInspected = elementInspected->next;
-            delete elementInspected;
-            elementInspected = nextInspected;
-        }
-        delete elementInspected;
-        nextInspected = NULL;
-        elementInspected = NULL;
-    }
-}
 
 void showBool(bool val){
 	if(val)
@@ -188,7 +64,6 @@ void showBool(bool val){
 		cout << "false" << endl;
 }
 
-
 bool isCommand(const string command,const char *mnemonic){
 	return command==mnemonic;
 }
@@ -196,8 +71,8 @@ bool isCommand(const string command,const char *mnemonic){
 int main(){
 	string line;
 	string command;
-	List *list=NULL;
-	int currentL=0;
+	Queue *queue=NULL;
+	int currentQ=0;
 	int value;
 	cout << "START" << endl;
 	while(true){
@@ -222,100 +97,59 @@ int main(){
 			break;
 		}
 
-
 		// zero-argument command
-		if(isCommand(command,"DH"))
+		if(isCommand(command,"EM"))
 		{
-			int retValue;
-			bool retBool=deleteHead(list[currentL],retValue);
-			if(retBool)
-				cout << retValue << endl;
-			else
-				showBool(retBool);
+			showBool(isEmpty(queue[currentQ]));
 			continue;
 		}
-		if(isCommand(command,"DT"))
+		if(isCommand(command,"FU"))
 		{
-			int retValue;
-			bool retBool=deleteTail(list[currentL],retValue);
-			if(retBool)
-				cout << retValue << endl;
-			else
-				showBool(retBool);
+			showBool(isFull(queue[currentQ]));
 			continue;
 		}
-
 		if(isCommand(command,"SH"))
 		{
-			showListFromHead(list[currentL]);
+			show(queue[currentQ]);
 			continue;
 		}
-
-		if(isCommand(command,"CL"))
+		if(isCommand(command,"DE"))
 		{
-			clearList(list[currentL]);
-			continue;
-		}
-
-		if(isCommand(command,"IN"))
-		{
-			init(list[currentL]);
+			int ret;
+			bool retBool=dequeue(queue[currentQ],ret);
+			if(!retBool)
+				cout << "false" << endl;
+			else
+				cout << ret << endl;
 			continue;
 		}
 
 		// read next argument, one int value
 		stream >> value;
 
-		if(isCommand(command,"FP"))
-		{
-			int ret;
-			ret=findPosOfValue(list[currentL],value);
-			cout << ret << endl;
-			continue;
-		}
-
-		if(isCommand(command,"DV"))
-		{
-			showBool(deleteValue(list[currentL],value));
-			continue;
-		}
-
-
-		if(isCommand(command,"AT"))
-		{
-			int retValue;
-			bool retBool=atPosition(list[currentL],value,retValue);
-			if(retBool)
-				cout << retValue << endl;
-			else
-				showBool(retBool);
-			continue;
-		}
-
-		if(isCommand(command,"CH"))
-		{
-			currentL=value;
-			continue;
-		}
-
-		if(isCommand(command,"IH"))
-		{
-			insertHead(list[currentL],value);
-			continue;
-		}
-
-		if(isCommand(command,"IT"))
-		{
-			insertTail(list[currentL],value);
-			continue;
-		}
-
 		if(isCommand(command,"GO"))
 		{
-			list=new List[value];
+			queue=new Queue[value];
+			continue;
+		}
+		if(isCommand(command,"CH"))
+		{
+			currentQ=value;
 			continue;
 		}
 
+		if(isCommand(command,"IN"))
+		{
+			init(queue[currentQ],value);
+			continue;
+		};
+
+		if(isCommand(command,"EN"))
+		{
+			bool retBool=enqueue(queue[currentQ],value);
+			showBool(retBool);
+			continue;
+		}
 		cout << "wrong argument in test: " << command << endl;
 	}
 }
